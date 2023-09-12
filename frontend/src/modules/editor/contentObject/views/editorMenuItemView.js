d9c733b98d410cfa5abe4d021faa83cc296888b5
pKarwingC <peter_themaster1210@hotmail.com>
2023-09-12 13:42:51 -0400

    fixed delete action error message popup issue for content objects

--- frontend/src/modules/editor/contentObject/views/editorMenuItemView.js
+++ frontend/src/modules/editor/contentObject/views/editorMenuItemView.js
@@ -125,10 +125,11 @@ define(function(require){
           Origin.trigger('editorView:itemDeleted', model);
           this.remove()
         }, this),
-        error: function(error) {
+        error: function(model, response) {
+          var errorMessage = response && typeof response == 'object' && response.responseJSON && response.responseJSON.message ? response.responseJSON.message : Origin.l10n.t('app.errordelete');
           Origin.Notify.alert({
             type: 'error',
-            text: 'app.errordelete'
+            text: errorMessage
           });
         }
       });

367b9b2f568b2d580590b37989ce3589fdc570ec
petchen123 <peterchen_karwing@outlook.com>
2023-10-16 11:18:15 -0400

    added login:changed event to the listener for help dialog

--- frontend/src/modules/helpDialog/index.js
+++ frontend/src/modules/helpDialog/index.js
@@ -4,7 +4,7 @@ define([
   '../../plugins/helpDialog/models/helpDialogManagementModel'], 
   function(Origin, HelpDialogView, HelpDialogManagementModel) {
     var helpDialogManagementModel = new HelpDialogManagementModel();
-    Origin.on('origin:dataReady helpDialogManagementSidebar:views:saved', function() {
+    Origin.on('origin:dataReady login:changed helpDialogManagementSidebar:views:saved', function() {
       helpDialogManagementModel.fetch({
         success: function(result) {
           $('.help-dialog').remove();

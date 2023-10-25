2a1a19eac84d3442e543195f12093e01ae2467ff
pKarwingC <peter_themaster1210@hotmail.com>
2023-10-25 13:49:40 -0400

    added bypass block for main content and help dialog

---
+++ frontend/src/plugins/bypassblocks/views/bypassBlockView.js
@@ -0,0 +1,49 @@
+// LICENCE https://github.com/adaptlearning/adapt_authoring/blob/master/LICENSE
+define(function(require){
+  var OriginView = require('core/views/originView');
+  var Origin = require('core/origin');
+
+  var BypassBlockView = OriginView.extend({
+    tagName: 'div',
+    className: 'bypass-block-content',
+
+    initialize: function() {
+      this.render();
+    },
+
+    events: {
+      'keydown .button--skip-link': 'skipToLink',
+      'click .button--skip-link': 'skipToLink'
+    },
+
+    skipToLink: function(e) {
+      e.preventDefault();
+
+      if (e.type == 'click' || (e.type == 'keydown' && e.which == 13)) {
+        var href = $(e.target).attr('href').substring(1);
+  
+        if (href == 'contentPane') {
+          $('.' + href).focus();
+        }
+        else {
+          $('#' + href).focus();
+        }
+      }
+
+      if ((e.type == 'keydown' && e.which == 9)) {
+        var $focusEl = e.shiftKey ? $(e.target).prevAll(':focusable:first') : $(e.target).nextAll(':focusable:first');
+        if ($focusEl.length > 0) {
+          $focusEl.focus();
+        }
+        else {
+          Origin.trigger('skip-focus-to-nav');
+        }
+      }
+    }
+    
+  }, {
+    template: 'bypassBlock'
+  });
+
+  return BypassBlockView;
+});

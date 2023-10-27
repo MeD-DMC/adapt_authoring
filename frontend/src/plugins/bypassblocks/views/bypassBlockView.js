a7a1e44394210c6c760f4a7fe22d93e7118cb853
petchen123 <peterchen_karwing@outlook.com>
2023-10-27 14:00:41 -0400

    fixed keyboard trap focus issue

--- frontend/src/plugins/bypassblocks/views/bypassBlockView.js
+++ frontend/src/plugins/bypassblocks/views/bypassBlockView.js
@@ -12,27 +12,13 @@ define(function(require){
     },
 
     events: {
-      'keydown .button--skip-link': 'skipToLink',
       'click .button--skip-link': 'skipToLink'
     },
 
     skipToLink: function(e) {
       e.preventDefault();
-
-      if (e.type == 'click' || (e.type == 'keydown' && e.which == 13)) {
-        var href = $(e.target).attr('href').substring(1);
-        $('#' + href).focus();
-      }
-
-      if ((e.type == 'keydown' && e.which == 9)) {
-        var $focusEl = e.shiftKey ? $(e.target).prevAll(':focusable:first') : $(e.target).nextAll(':focusable:first');
-        if ($focusEl.length > 0) {
-          $focusEl.focus();
-        }
-        else {
-          $('.general-ribbon').length > 0 ? $('.ribbon-inner').focus() : $('.navigation-global-menu').focus();
-        }
-      }
+      var href = $(e.target).attr('href').substring(1);
+      $('#' + href).focus();
     }
     
   }, {

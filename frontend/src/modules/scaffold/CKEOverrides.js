fde2bc5ad8504d82e2151823f8a1071a9d3d889f
ftourangeau <francis.tourangeau@hrsdc-rhdcc.gc.ca>
2024-02-05 12:51:51 -0500

    removed ck4 references

--- frontend/src/modules/scaffold/CKEOverrides.js
+++
@@ -1,9 +0,0 @@
-define([
-  'core/origin'
-], function () {
-  CKEDITOR.ui.prototype.space = function(name) {
-    var id = '#' + this.spaceId(name);
-    var spaceElem = this.editor.container.findOne(id);
-    return spaceElem;
-  }
-});

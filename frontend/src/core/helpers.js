8f081b6e72eaa4ab97e474e415dcb9e83e585710
ftourangeau <francis.tourangeau@hrsdc-rhdcc.gc.ca>
2024-02-26 13:08:03 -0500

    basic implementation of extra info on fields

--- frontend/src/core/helpers.js
+++ frontend/src/core/helpers.js
@@ -72,7 +72,7 @@ define(function(require){
       if(options.parent){
       l10nKey += `${options.parent}.`
       }
-      
+
       l10nKey += `${options.key}.`
       l10nKey += `${options.type}`
 
@@ -141,6 +141,14 @@ define(function(require){
       var sum = '';
       for (var i = 1; i <= n; ++i) sum += block.fn(i);
       return sum;
+    },
+
+    ifEquals: function(arg1, arg2, options){
+      return (arg1 == arg2) ? options.fn(this) : options.inverse(this);
+    },
+
+    ifNotEquals: function(arg1, arg2, options){
+      return (arg1 != arg2) ? options.fn(this) : options.inverse(this);
     },
 
     t: function(str, options) {
@@ -417,7 +425,7 @@ define(function(require){
         hex = hex.replace(shorthandRegex, function(m, r, g, b) {
           return r + r + g + g + b + b;
         });
-      
+
         var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
         return result ? {
           r: parseInt(result[1], 16),
@@ -425,8 +433,8 @@ define(function(require){
           b: parseInt(result[3], 16)
         } : null;
       }
-      
-      // function from https://stackoverflow.com/a/9733420/3695983                     
+
+      // function from https://stackoverflow.com/a/9733420/3695983
       function luminance(r, g, b) {
         var a = [r, g, b].map(function (v) {
           v /= 255;
@@ -436,20 +444,20 @@ define(function(require){
         });
         return a[0] * 0.2126 + a[1] * 0.7152 + a[2] * 0.0722;
       }
-      
+
       function calculateRatio(color1, color2) {
         const color1rgb = hexToRgb(color1);
         const color2rgb = hexToRgb(color2);
-      
+
         // calculate the relative luminance
         const color1luminance = luminance(color1rgb.r, color1rgb.g, color1rgb.b);
         const color2luminance = luminance(color2rgb.r, color2rgb.g, color2rgb.b);
-      
+
         // calculate the color contrast ratio
-        const ratio = color1luminance > color2luminance 
+        const ratio = color1luminance > color2luminance
         ? ((color2luminance + 0.05) / (color1luminance + 0.05))
         : ((color1luminance + 0.05) / (color2luminance + 0.05));
-      
+
         return ratio;
       }
       var ratio = calculateRatio(color1, color2)
--- frontend/src/modules/scaffold/index.js
+++ frontend/src/modules/scaffold/index.js
@@ -16,7 +16,7 @@ define([
   './views/scaffoldUsersView',
   './views/scaffoldSingleUserView',
   './views/scaffoldUsersWithEmailView'
-], function(Origin, Helpers, Schemas, BackboneForms, BackboneFormsLists, BackboneFormsOverrides, ScaffoldAssetView, ScaffoldAssetItemView, ScaffoldCodeEditorView, ScaffoldColourPickerView, ScaffoldDisplayTitleView, ScaffoldItemsModalView, ScaffoldListView, ScaffoldTagsView, ScaffoldUsersView, ScaffoldSingleUserView, ScaffoldUsersWithEmailView) {
+], function (Origin, Helpers, Schemas, BackboneForms, BackboneFormsLists, BackboneFormsOverrides, ScaffoldAssetView, ScaffoldAssetItemView, ScaffoldCodeEditorView, ScaffoldColourPickerView, ScaffoldDisplayTitleView, ScaffoldItemsModalView, ScaffoldListView, ScaffoldTagsView, ScaffoldUsersView, ScaffoldSingleUserView, ScaffoldUsersWithEmailView) {
 
   var Scaffold = {};
   var alternativeModel;
@@ -32,11 +32,11 @@ define([
   Backbone.Form.editors.List.Modal.ModalAdapter = ScaffoldItemsModalView;
 
   function onScaffoldUpdateSchemas(callback, context) {
-    Origin.trigger('schemas:loadData', function() {
+    Origin.trigger('schemas:loadData', function () {
       callback.apply(context);
     });
   }
-// ESDC - added options input to function extra options and parent context
+  // ESDC - added options input to function extra options and parent context
   function generateFieldObject(field, key, options, parent) {
     var fieldType = field.type;
     var isFieldTypeObject = fieldType === 'object';
@@ -46,7 +46,7 @@ define([
     var itemsInputType = items && items.inputType;
     var confirmDelete = Origin.l10n.t('app.confirmdelete');
 
-    var getTitle = function() {
+    var getTitle = function () {
       var title = field.title;
 
       if (title) {
@@ -58,7 +58,7 @@ define([
       }
     };
 
-    var getType = function() {
+    var getType = function () {
       if (inputType) {
         return inputType;
       }
@@ -76,7 +76,7 @@ define([
       }
     };
 
-    var getValidators = function() {
+    var getValidators = function () {
       var validators = field.validators || [];
 
       for (var i = 0, j = validators.length; i < j; i++) {
@@ -106,254 +106,266 @@ define([
       return validators.filter(Boolean);
     };
     // ESDC - added local function to build item keys
-    var buildTranslationKey = function(type, fallback){
-      if(parent === '_items'){
+    var buildTranslationKey = function (type, fallback) {
+      if (parent === '_items') {
         parent = options.model.attributes._component;
       }
 
-      if(key === '_items'){
+      if (key === '_items') {
         key = `${key}.${options.model.attributes._component}`
       }
 
       var level = options.level || 'all';
 
-      return {key: key, level: level, parent: parent, type: type, fallback: fallback}
+      return { key: key, level: level, parent: parent, type: type, fallback: fallback }
     }
 
     // ESDC - added Label fields to use in handlebars templates
     // ESDC - get translated labels from generated key or return original label
-    var fieldObject = {
-      confirmDelete: itemsProperties ? confirmDelete : field.confirmDelete,
-      default: field.default,
-      editorAttrs: field.editorAttrs,
-      editorClass: field.editorClass,
-      fieldAttrs: field.fieldAttrs,
-      fieldClass: field.fieldClass,
-      help: field.help,
-      helpLabel: Helpers.keyToTranslatedString(buildTranslationKey('help', field.help)) || field.help,
-      itemType: itemsProperties ? 'Object' : itemsInputType,
-      inputType: inputType,
-      legend: field.legend,
-      legendLabel: Helpers.keyToTranslatedString(buildTranslationKey('legend', field.legend)) || field.legend,
-      subSchema: isFieldTypeObject ? field.properties : itemsProperties || items,
-      title: getTitle(),
-      titleHTML: field.titleHTML,
-      titleLabel: Helpers.keyToTranslatedString(buildTranslationKey('label', getTitle())) || getTitle(),
-      type: getType(),
-      validators: getValidators(),
-      a11y: field.a11y,
-      secondaryLabel: field.secondaryLabel
-    };
 
-    if (_.isObject(inputType)) {
-      // merge nested inputType attributes into fieldObject
-      fieldObject = _.extend(fieldObject, inputType);
+    function getTypeLabel() {
+      var type = getType();
+      if (typeof type === 'object' || Array.isArray(type) || type === null) {
+        return 'Other'
+      }
+      return type
+    }
+    console.log(getTypeLabel());
+      var fieldObject = {
+        confirmDelete: itemsProperties ? confirmDelete : field.confirmDelete,
+        default: field.default,
+        editorAttrs: field.editorAttrs,
+        editorClass: field.editorClass,
+        fieldAttrs: field.fieldAttrs,
+        fieldClass: field.fieldClass,
+        help: field.help,
+        helpLabel: Helpers.keyToTranslatedString(buildTranslationKey('help', field.help)) || field.help,
+        itemType: itemsProperties ? 'Object' : itemsInputType,
+        inputType: inputType,
+        legend: field.legend,
+        legendLabel: Helpers.keyToTranslatedString(buildTranslationKey('legend', field.legend)) || field.legend,
+        subSchema: isFieldTypeObject ? field.properties : itemsProperties || items,
+        title: getTitle(),
+        titleHTML: field.titleHTML,
+        titleLabel: Helpers.keyToTranslatedString(buildTranslationKey('label', getTitle())) || getTitle(),
+        type: getType(),
+        fieldType: getTypeLabel(),
+        validators: getValidators(),
+        a11y: field.a11y,
+        secondaryLabel: field.secondaryLabel
+      };
+
+      //console.log(fieldObject);
+
+      if (_.isObject(inputType)) {
+        // merge nested inputType attributes into fieldObject
+        fieldObject = _.extend(fieldObject, inputType);
+      }
+
+      return fieldObject;
     }
 
-    return fieldObject;
-  }
-
-  // ESDC - added inputs to function for extra options and parent
-  // ESDC - added parent value generation for fields label if not parent was passed
-  function setUpSchemaFields(field, key, schema, scaffoldSchema, options, parent) {
-    if(!parent){
-      if(schema[key].isSetting){
-        parent = 'settings';
-      } else if(schema[key].type !== 'object'){
-        parent = 'general';
-      } else {
-        parent = key;
-      }
-    }
-    scaffoldSchema[key] = generateFieldObject(field, key, options, parent);
-
-    var objectSchema = schema[key].properties || schema[key].subSchema;
-    var scaffoldObjectSchema = scaffoldSchema[key].subSchema;
-
-    for (var i in objectSchema) {
-      if (objectSchema.hasOwnProperty(i)) {
-        // ESDC - added extra options and key to field generation
-        setUpSchemaFields(objectSchema[i], i, objectSchema, scaffoldObjectSchema, options, key);
-      }
-    }
-  }
-
-  function buildSchema(schema, options, type) {
-
-    var scaffoldSchema = {};
-
-    for (var key in schema) {
-      if (!schema.hasOwnProperty(key)) continue;
-
-      var field = schema[key];
-      var nestedProps = field.properties;
-
-      if (!options.isTheme || !nestedProps) {
-        // ESDC - added extra options to field generation
-        setUpSchemaFields(field, key, schema, scaffoldSchema, options);
-        continue;
-      }
-
-      // process nested properties on edit theme page
-      for (var innerKey in nestedProps) {
-        if (!nestedProps.hasOwnProperty(innerKey)) continue;
-        // ESDC - added extra options and key to field generation
-        setUpSchemaFields(nestedProps[innerKey], innerKey, nestedProps, scaffoldSchema, options, key);
-      }
-    }
-
-    return scaffoldSchema;
-  }
-
-  function buildFieldsets(schema, options) {
-    var fieldsets = {
-      general: { key: 'general', legend: Origin.l10n.t('app.scaffold.general'), fields: [] },
-      properties: { key: 'properties', legend: Origin.l10n.t('app.scaffold.properties'), fields: [] },
-      settings: { key: 'settings', legend: Origin.l10n.t('app.scaffold.settings'), fields: [] },
-      extensions: { key: 'extensions', legend: Origin.l10n.t('app.scaffold.extensions'), fields: [ '_extensions' ] }
-    };
-
-    for (var key in schema) {
-      if (!schema.hasOwnProperty(key) || key === '_extensions') continue;
-
-      var value = schema[key];
-
-      if (value.isSetting) {
-        fieldsets.settings.fields.push(key);
-        continue;
-      }
-
-      if (value.type !== 'object') {
-        fieldsets.general.fields.push(key);
-        continue;
-      }
-
-      if (fieldsets[key]) {
-        fieldsets[key].fields.push(key);
-        continue;
-      }
-
-      var nestedProps = value.properties;
-      var fields = [];
-
-      // process nested properties on edit theme page
-      if (options.isTheme) {
-        for (var innerKey in nestedProps) {
-          if (nestedProps.hasOwnProperty(innerKey)) {
-            fields.push(innerKey);
-          }
+    // ESDC - added inputs to function for extra options and parent
+    // ESDC - added parent value generation for fields label if not parent was passed
+    function setUpSchemaFields(field, key, schema, scaffoldSchema, options, parent) {
+      if (!parent) {
+        if (schema[key].isSetting) {
+          parent = 'settings';
+        } else if (schema[key].type !== 'object') {
+          parent = 'general';
+        } else {
+          parent = key;
         }
       }
-      // ESDC - modified fieldset legend value to use translated string first
-      fieldsets[key] = {
-        key: key,
-        legend: Helpers.keyToTitleString(key) || value.title,
-        fields: fields.length ? fields : [ key ]
+      scaffoldSchema[key] = generateFieldObject(field, key, options, parent);
+
+      var objectSchema = schema[key].properties || schema[key].subSchema;
+      var scaffoldObjectSchema = scaffoldSchema[key].subSchema;
+
+      for (var i in objectSchema) {
+        if (objectSchema.hasOwnProperty(i)) {
+          // ESDC - added extra options and key to field generation
+          setUpSchemaFields(objectSchema[i], i, objectSchema, scaffoldObjectSchema, options, key);
+        }
+      }
+    }
+
+    function buildSchema(schema, options, type) {
+
+      var scaffoldSchema = {};
+
+      for (var key in schema) {
+        if (!schema.hasOwnProperty(key)) continue;
+
+        var field = schema[key];
+        var nestedProps = field.properties;
+
+        if (!options.isTheme || !nestedProps) {
+          // ESDC - added extra options to field generation
+          setUpSchemaFields(field, key, schema, scaffoldSchema, options);
+          continue;
+        }
+
+        // process nested properties on edit theme page
+        for (var innerKey in nestedProps) {
+          if (!nestedProps.hasOwnProperty(innerKey)) continue;
+          // ESDC - added extra options and key to field generation
+          setUpSchemaFields(nestedProps[innerKey], innerKey, nestedProps, scaffoldSchema, options, key);
+        }
+      }
+
+      return scaffoldSchema;
+    }
+
+    function buildFieldsets(schema, options) {
+      var fieldsets = {
+        general: { key: 'general', legend: Origin.l10n.t('app.scaffold.general'), fields: [] },
+        properties: { key: 'properties', legend: Origin.l10n.t('app.scaffold.properties'), fields: [] },
+        settings: { key: 'settings', legend: Origin.l10n.t('app.scaffold.settings'), fields: [] },
+        extensions: { key: 'extensions', legend: Origin.l10n.t('app.scaffold.extensions'), fields: ['_extensions'] }
       };
+
+      for (var key in schema) {
+        if (!schema.hasOwnProperty(key) || key === '_extensions') continue;
+
+        var value = schema[key];
+
+        if (value.isSetting) {
+          fieldsets.settings.fields.push(key);
+          continue;
+        }
+
+        if (value.type !== 'object') {
+          fieldsets.general.fields.push(key);
+          continue;
+        }
+
+        if (fieldsets[key]) {
+          fieldsets[key].fields.push(key);
+          continue;
+        }
+
+        var nestedProps = value.properties;
+        var fields = [];
+
+        // process nested properties on edit theme page
+        if (options.isTheme) {
+          for (var innerKey in nestedProps) {
+            if (nestedProps.hasOwnProperty(innerKey)) {
+              fields.push(innerKey);
+            }
+          }
+        }
+        // ESDC - modified fieldset legend value to use translated string first
+        fieldsets[key] = {
+          key: key,
+          legend: Helpers.keyToTitleString(key) || value.title,
+          fields: fields.length ? fields : [key]
+        };
+      }
+
+      if (!schema._extensions) {
+        delete fieldsets.extensions;
+      }
+
+      if (!fieldsets.settings.fields.length) {
+        delete fieldsets.settings;
+      }
+
+      if (!fieldsets.properties.fields.length) {
+        delete fieldsets.properties;
+      }
+
+      return _.values(fieldsets);
     }
 
-    if (!schema._extensions) {
-      delete fieldsets.extensions;
-    }
+    Scaffold.buildForm = function (options) {
+      var model = options.model;
+      var type = model.get('_type') || model._type || options.schemaType;
+      options.isTheme = false;
 
-    if (!fieldsets.settings.fields.length) {
-      delete fieldsets.settings;
-    }
+      switch (type) {
+        case 'menu':
+        case 'page':
+          type = 'contentobject';
+          break;
+        case 'component':
+          type = model.get('_component');
+          break;
+        case 'theme':
+          type = options.schemaType;
+          options.isTheme = true;
+      }
 
-    if (!fieldsets.properties.fields.length) {
-      delete fieldsets.properties;
-    }
+      var schema = new Schemas(type);
+      if (options.isTheme) {
+        schema = schema.variables;
+      }
+      options.model.schema = buildSchema(schema, options, type);
+      options.fieldsets = buildFieldsets(schema, options);
+      alternativeModel = options.alternativeModelToSave;
+      alternativeAttribute = options.alternativeAttributeToSave;
+      currentModel = options.model;
+      currentForm = new Backbone.Form(options).render();
 
-    return _.values(fieldsets);
-  }
-
-  Scaffold.buildForm = function(options) {
-    var model = options.model;
-    var type = model.get('_type') || model._type || options.schemaType;
-    options.isTheme = false;
-
-    switch (type) {
-      case 'menu':
-      case 'page':
-        type = 'contentobject';
-        break;
-      case 'component':
-        type = model.get('_component');
-        break;
-      case 'theme':
-        type = options.schemaType;
-        options.isTheme = true;
-    }
-
-    var schema = new Schemas(type);
-    if (options.isTheme) {
-      schema = schema.variables;
-    }
-    options.model.schema = buildSchema(schema, options, type);
-    options.fieldsets = buildFieldsets(schema, options);
-    alternativeModel = options.alternativeModelToSave;
-    alternativeAttribute = options.alternativeAttributeToSave;
-    currentModel = options.model;
-    currentForm = new Backbone.Form(options).render();
-
-    return currentForm;
-  };
-
-  Scaffold.addCustomField = function(fieldName, view, overwrite) {
-    if (Backbone.Form.editors[fieldName] && !overwrite) {
-      console.log('Sorry, the custom field you’re trying to add already exists');
-    } else {
-      Backbone.Form.editors[fieldName] = view;
-    }
-  };
-
-  Scaffold.addCustomTemplate = function(templateName, template, overwrite) {
-    if (!templateName || !template) {
-      return console.log('Custom templates need a name and template');
-    }
-
-    if (customTemplates[templateName] && !overwrite) {
-      console.log('Sorry, the custom template you’re trying to add already exists');
-    } else {
-      customTemplates[templateName] = template;
-    }
-  };
-
-  Scaffold.addCustomValidator = function(name, validatorMethod) {
-    if (!name || !validatorMethod) {
-      console.log('Custom validators need a name and validatorMethod');
-    } else {
-      customValidators.push({ name: name, validatorMethod: validatorMethod });
-    }
-  };
-
-  // example of customValidator
-  /*Scaffold.addCustomValidator('title', function(value, formValues) {
-    var err = {
-      type: 'username',
-      message: 'Usernames must be at least three characters long'
+      return currentForm;
     };
 
-    if (value.length < 3) return err;
-  });*/
+    Scaffold.addCustomField = function (fieldName, view, overwrite) {
+      if (Backbone.Form.editors[fieldName] && !overwrite) {
+        console.log('Sorry, the custom field you’re trying to add already exists');
+      } else {
+        Backbone.Form.editors[fieldName] = view;
+      }
+    };
 
-  Scaffold.getCurrentModel = function() { return currentModel; };
-  Scaffold.getCurrentForm = function() { return currentForm; };
-  Scaffold.getAlternativeModel = function() { return alternativeModel; };
-  Scaffold.getAlternativeAttribute = function() { return alternativeAttribute; };
-  Scaffold.getCurrentActiveModals = function() { return ActiveItemsModal; };
-  Scaffold.isOverlayActive = function() { return isOverlayActive; };
-  Scaffold.setOverlayActive = function(booleanValue) { isOverlayActive = booleanValue; };
-  Scaffold.setModel = function(model) { currentModel = model; };
-  Scaffold.addCustomField('Boolean', Backbone.Form.editors.Checkbox);
-  Scaffold.addCustomField('QuestionButton', Backbone.Form.editors.Text);
+    Scaffold.addCustomTemplate = function (templateName, template, overwrite) {
+      if (!templateName || !template) {
+        return console.log('Custom templates need a name and template');
+      }
 
-  Origin.on({
-    'scaffold:updateSchemas': onScaffoldUpdateSchemas,
-    'scaffold:increaseActiveModals': function() { ActiveItemsModal++; },
-    'scaffold:decreaseActiveModals': function() { ActiveItemsModal--; },
+      if (customTemplates[templateName] && !overwrite) {
+        console.log('Sorry, the custom template you’re trying to add already exists');
+      } else {
+        customTemplates[templateName] = template;
+      }
+    };
+
+    Scaffold.addCustomValidator = function (name, validatorMethod) {
+      if (!name || !validatorMethod) {
+        console.log('Custom validators need a name and validatorMethod');
+      } else {
+        customValidators.push({ name: name, validatorMethod: validatorMethod });
+      }
+    };
+
+    // example of customValidator
+    /*Scaffold.addCustomValidator('title', function(value, formValues) {
+      var err = {
+        type: 'username',
+        message: 'Usernames must be at least three characters long'
+      };
+
+      if (value.length < 3) return err;
+    });*/
+
+    Scaffold.getCurrentModel = function () { return currentModel; };
+    Scaffold.getCurrentForm = function () { return currentForm; };
+    Scaffold.getAlternativeModel = function () { return alternativeModel; };
+    Scaffold.getAlternativeAttribute = function () { return alternativeAttribute; };
+    Scaffold.getCurrentActiveModals = function () { return ActiveItemsModal; };
+    Scaffold.isOverlayActive = function () { return isOverlayActive; };
+    Scaffold.setOverlayActive = function (booleanValue) { isOverlayActive = booleanValue; };
+    Scaffold.setModel = function (model) { currentModel = model; };
+    Scaffold.addCustomField('Boolean', Backbone.Form.editors.Checkbox);
+    Scaffold.addCustomField('QuestionButton', Backbone.Form.editors.Text);
+
+    Origin.on({
+      'scaffold:updateSchemas': onScaffoldUpdateSchemas,
+      'scaffold:increaseActiveModals': function () { ActiveItemsModal++; },
+      'scaffold:decreaseActiveModals': function () { ActiveItemsModal--; },
+    });
+
+    Origin.scaffold = Scaffold;
+
   });
-
-  Origin.scaffold = Scaffold;
-
-});

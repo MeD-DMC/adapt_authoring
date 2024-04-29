14d98407422fc853b79dd3f5bb8c824e2d4e4ec2
ftourangeau <francis.tourangeau@hrsdc-rhdcc.gc.ca>
2024-04-29 17:28:07 -0400

    WIP collapsible sidebar menu

--- frontend/src/modules/sidebar/views/sidebarView.js
+++ frontend/src/modules/sidebar/views/sidebarView.js
@@ -11,6 +11,7 @@ define(function(require) {
 
 		initialize: function() {
 			this.render();
+      this.$el.find('.collapse-sidebar').on('click', this.toggleSidebar);
 			this.listenTo(Origin, 'sidebar:sidebarContainer:update', this.updateViews);
 			this.listenTo(Origin, 'sidebar:sidebarFilter:add', this.addFilterView);
 			this.listenTo(Origin, 'sidebar:sidebarContainer:hide', this.hideSidebar);
@@ -61,7 +62,36 @@ define(function(require) {
 		addFilterView: function(options) {
 			Origin.trigger('sidebar:sidebarFilter:remove');
 			$('body').append(new SidebarFilterView(options).$el);
-		}
+		},
+
+    toggleSidebar: function() {
+      var contentWrapper = $('#content_wrapper');
+      var collapseIcon = $('.collapse-sidebar i');
+      var sidebarInner = $('.sidebar-inner');
+
+      if (contentWrapper.hasClass('folded')) {
+        contentWrapper.removeClass('folded');
+        collapseIcon.addClass('fa-chevron-circle-left').removeClass('fa-chevron-circle-right');
+        sidebarInner.removeClass('display-none').velocity({ 'left': '0%', 'opacity': 1 }, {
+          duration: 200,
+          easing: "easeOutQuad"
+        });
+      } else {
+        contentWrapper.addClass('folded');
+        collapseIcon.removeClass('fa-chevron-circle-left').addClass('fa-chevron-circle-right');
+        sidebarInner.velocity({ 'left': '0%', 'opacity': 0 }, {
+          duration: 200,
+          easing: "easeOutQuad",
+          complete: function() {
+            var that = this;
+            setTimeout(function() {
+              window.dispatchEvent(new Event('resize'));
+              $(that).addClass('display-none');
+            }, 250);
+          }
+        });
+      }
+    }
 	}, {
 		template: 'sidebar'
 	});

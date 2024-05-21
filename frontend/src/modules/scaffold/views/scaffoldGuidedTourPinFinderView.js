6de94a50ad60437f670aeba2195d2bbc8c3cfffc
ftourangeau <francis.tourangeau@hrsdc-rhdcc.gc.ca>
2024-05-21 16:43:36 -0400

    added pin finder validator for image selected

--- frontend/src/modules/scaffold/views/scaffoldGuidedTourPinFinderView.js
+++ frontend/src/modules/scaffold/views/scaffoldGuidedTourPinFinderView.js
@@ -12,14 +12,71 @@ define(['core/origin', 'backbone-forms'], function (Origin, BackboneForms) {
       this.template = options.template || this.constructor.template;
     },
 
+    preRender: function () {
+      this.imageReady = this.form && this.form.data && this.form.data._graphic && this.form.data._graphic.src;
+    },
+
     render: function () {
-      this.$el.append(Handlebars.templates[this.constructor.template]({}));
+      this.preRender();
+      this.$el.append(Handlebars.templates[this.constructor.template](this));
+      this.postRender();
       return this;
     },
 
-    editPinPosition: function(e){
+    postRender: function () {
+      if(this.form && this.form.fields && this.form.fields._graphic && this.form.fields._graphic.$el){
+        var _graphic_src = this.form.fields._graphic.$el.find('#_graphic_src')[0];
+        this.startObserver(_graphic_src);
+      }
+    },
+
+    startObserver: function (targetNode) {
+      var self = this;
+      this.observer = new MutationObserver(function (mutationsList) {
+        for (var mutation of mutationsList) {
+          if (mutation.type === 'childList') {
+            mutation.addedNodes.forEach(function (node) {
+              if (node.nodeName.toLowerCase() === 'img') {
+                console.log('An img was inserted');
+                self.enablePinEditor();
+              }
+            });
+            mutation.removedNodes.forEach(function (node) {
+              if (node.nodeName.toLowerCase() === 'img') {
+                console.log('An img was removed');
+                self.disablePinEditor();
+              }
+            });
+          }
+        }
+      });
+      this.observer.observe(targetNode, { childList: true, subtree: true });
+    },
+
+    enablePinEditor: function () {
+      this.imageReady = true;
+      this.$el.find('.edit-pin-position').removeClass('disabled');
+      this.$el.find('.field-error').text('');
+    },
+
+    disablePinEditor: function () {
+      this.imageReady = false;
+      this.$el.find('.edit-pin-position').addClass('disabled');
+    },
+
+    editPinPosition: function (e) {
       e.preventDefault();
-      Origin.trigger('guidedtourpinfinder:open', this);
+      if (this.imageReady) {
+        Origin.trigger('guidedtourpinfinder:open', this);
+      } else {
+        var errorMsg = Origin.l10n.t('app.pinfinder.error') || 'You must select an image as graphic src to use the pin editor.';
+        this.$el.find('.field-error').text(errorMsg);
+      }
+    },
+
+    remove: function () {
+      this.observer.disconnect();
+      Backbone.View.prototype.remove.apply(this, arguments);
     }
 
   }, { template: 'scaffoldGuidedTourPinFinder' });
